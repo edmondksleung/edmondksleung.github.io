bbb8f2c03fa05ce8120273c0c7107195262826b8
edmondksleung <edmondksleung@hotmail.com>
2020-09-26 11:24:26 -0700

    fixed more bugs with Part A

--- comp_4537/labs/lab2/partA.js
+++ comp_4537/labs/lab2/partA.js
@@ -1,13 +1,12 @@
 function generateButtons() 
 {
     let input = document.getElementById("numLetters").value;
-    let buttonArea = document.getElementById("buttonArea");
-
-    buttonArea.innerHTML = "";
+    let letterArea = document.getElementById("letterArea");
 
     let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
         "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+    letterArea.innerHTML = "";
 
     if (input < 0 || input > 26) 
     {
@@ -19,7 +18,7 @@ function generateButtons()
     {
         let button = document.createElement("button");
         button.innerHTML = alphabet[i];
-        buttonArea.appendChild(button);
+        letterArea.appendChild(button);
 
         button.addEventListener ("click", function() {
             console.log("Button " + alphabet[i] + " was pressed!" )
