93c2f3154e6e036bed534a3c3c6dff879463fba7
Edmond Leung <edmondksleung@hotmail.com>
2020-10-10 20:54:04 -0700

    Added serverTime.html and thisQuestions.html

---
+++ comp_4537/labs/lab3/server.js
@@ -0,0 +1,9 @@
+let http = require('http');
+let url = require('url');
+let time = require('./myModule');
+http.createServer(function (req, res) {
+  let q = url.parse(req.url, true);
+  console.log(q.query); //returns '?name=John'
+  res.writeHead(200, {"Content-Type": "text/html", "Access-Control-Allow-Origin": "*"});
+  res.end('Hello '+q.query["name"] + ', the current server time is ' + time.myTime());
+}).listen(8888);
