80c00e5be4c10b1a02296c973b28052f1c574353
edmondksleung <edmondksleung@hotmail.com>
2020-10-30 01:10:03 -0700

    grid cells now have classes incorrect and correct

--- comp_4537/memorygame/memorygame.js
+++ comp_4537/memorygame/memorygame.js
@@ -1,35 +1,59 @@
 function createGrid(x, y) {
+    let id = 0;
+
+    var correctGrids = random(x, y)
+
     for (var cols = 0; cols < x; cols++) {
-        for (var rows = 0; rows< y; rows++) {
-            $("#playable").append("<div class='grid'></div>");
+        for (var rows = 0; rows < y; rows++) {
+
+            if (correctGrids.includes(id)) {
+                var item = "<div id=g" + id + " class='grid correct'></div>";
+            } else {
+                var item = "<div id=g" + id + " class='grid incorrect'></div>";
+            }
+        
+            $("#playable").append(item);
+            id++;
         };
     };
     $("#playable").width(85 * x);
     $("#playable").height(85 * y);
 };
 
-// function that clears the grid
 function clearGrid(){
     $(".grid").remove();
 };  
 
-// function that prompts the user to select the number of boxes in a new grid
-// the function then also creates that new grid
 function refreshGrid(){
-    var z = prompt("How many boxes per side?");
     clearGrid();
-    createGrid(z, z);
+    createGrid(6, 4);
 };
+
+function random(x, y) {
+    var randomArray = [];
+
+    for (var i = 0; i < x; i++) {
+        var randomInt = Math.floor(Math.random() * (x * y));
+
+        if (randomArray.includes(randomInt)) {
+            var randomInt = Math.floor(Math.random() * (x * y));
+        }
+
+        randomArray.push(randomInt)
+    }
+    
+    return randomArray
+}
 
 // create a 16x16 grid when the page loads
 // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
 // allows the click of a button to prompt the user to create a new grid
 $(document).ready(function() {
-    createGrid(4,3);
+    createGrid(3,3);
 
-    $(".grid").mouseover(function() {
-        $(this).css("background-color", "black");
-        });
+   // $(".grid").mouseover(function() {
+   //     $(this).css("background-color", "black");
+   //     });
 
     $(".newGrid").click(function() {
         refreshGrid();
