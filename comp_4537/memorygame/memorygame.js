e5d12522acbee9dc7e8cedd63d9f74e22912dba7
edmondksleung <edmondksleung@hotmail.com>
2020-10-29 23:32:14 -0700

    grid dynamically appears

---
+++ comp_4537/memorygame/memorygame.js
@@ -0,0 +1,41 @@
+function createGrid(x, y) {
+    for (var cols = 0; cols < x; cols++) {
+        for (var rows = 0; rows< y; rows++) {
+            $("#playable").append("<div class='grid'></div>");
+        };
+    };
+    $("#playable").width(85 * x);
+    $("#playable").height(85 * y);
+};
+
+// function that clears the grid
+function clearGrid(){
+    $(".grid").remove();
+};  
+
+// function that prompts the user to select the number of boxes in a new grid
+// the function then also creates that new grid
+function refreshGrid(){
+    var z = prompt("How many boxes per side?");
+    clearGrid();
+    createGrid(z, z);
+};
+
+// create a 16x16 grid when the page loads
+// creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
+// allows the click of a button to prompt the user to create a new grid
+$(document).ready(function() {
+    createGrid(4,3);
+
+    $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+
+    $(".newGrid").click(function() {
+        refreshGrid();
+
+        $(".grid").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+    });
+});
